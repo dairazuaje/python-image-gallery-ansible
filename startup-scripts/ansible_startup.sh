6afa636cf9d5ee0f2c6b5948e5f18c7f493d78a6
EC2 Default User <ec2-user@ip-10-0-1-125.ec2.internal>
2020-06-29 17:09:43 +0000

    test

--- startup-scripts/ansible_startup.sh
+++ startup-scripts/ansible_startup.sh
@@ -5,11 +5,11 @@ yum install -y gcc
 yum install -y python3
 yum install -y python3-devel
 yum install -y postgresql-devel
-pip3 install boto3 --user
-pip3 install boto --user
-pip3 install ansible --user
-pip3 install psycopg2 --user
+pip3 install boto3 #--user
+pip3 install boto #--user
+pip3 install ansible #--user
+pip3 install psycopg2 #--user
 yum install -y git
 cd /home/ec2-user
 git clone https://github.com/dairazuaje/python-image-gallery-ansible.git
-chown -R ec2-user:ec2-user python-image-gallery
+chown -R ec2-user:ec2-user python-image-gallery-ansible
